515468de4d02c5ff762991bc0bd797d81c62cc13
Steve Beale <beale.stephen@gmail.com>
2024-08-29 14:12:39 +0100

    Setting up for authnticated users etc.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HPlus.Ecommerce.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        //Must check user is validated before proceeding
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
