9caa73e764e45c28a2a77c9836421f7730372eca
Steve Beale <beale.stephen@gmail.com>
2024-08-29 12:49:09 +0100

    Before lunch - gone on to Attribute routing video

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HPlus.Ecommerce.Controllers
+{
+    //Saves writing this for all in this class
+    [RoutePrefix("product")]
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [RoutePrefix("list")]
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HPlus.Ecommerce.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            //redirects user from /products to /product list page as a result of this
+            return RedirectToAction("Index", "Product");
+        }
+    }
+}
